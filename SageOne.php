40167a5074ef61b06b75a1ef2a6d739782e62689
Ryan Mitchell <ryanmitchell@thoughtcollective.com>
2013-05-23 14:24:39 +0100

    Fixes and simplification
    Made some amends to the auth method as it wasn't working  with Sage One
    as of today.
    
    Rather than defining set methods for resources I've set up get, post,
    put, delete methods which correspond to the API set up. These can then
    be called against any API resource.

--- SageOne.php
+++ SageOne.php
@@ -20,7 +20,7 @@ class SageOne {
     
     private $apiUrl = 'https://app.sageone.com/api/v1';
     
-    private $debug = true;
+    private $debug = false;
     
     function __construct($clientId, $clientSecret){
         
@@ -53,67 +53,33 @@ class SageOne {
         );
         
         $result = $this->call('', 'oauth', $params);
-        
-        
-        // Get array of accessToken
-        $accessToken = json_decode($result, true);
-        
+                
         // Save accessToken
-        $this->accessToken = $accessToken['accessToken'];
+        $this->accessToken = $result['access_token'];
     
         // Return the response as an array
-        return $accessToken;
+        return $result;
     }
     
     public function setAccessToken($accessToken){
         $this->accessToken = $accessToken;
     }
     
-    public function createContact($data){
-        
-        // Wrap params in array
-        $data = array("contact" => $data);
-        
-        $result = $this->post('/contacts', $data);
-        return $result;
+    public function get($endpoint, $data=false){
+        return $this->call($endpoint, 'get', $data);
     }
     
-    public function createInvoice($params){
-        
-        // Wrap params in array
-        $data = array("sales_invoice" => $params);
-        
-        $result = $this->post('/sales_invoices', $data);
-        return $result;
+    public function post($endpoint, $data){
+        return $this->call($endpoint, 'post', $data);
     }
     
-    public function createInvoicePayment($sales_invoice_id, $params){
-        
-        // Wrap params in array
-        $data = array("sales_invoice_payment" => $params);
-        
-        $result = $this->post('/sales_invoices/'.$sales_invoice_id.'/payments', $data);
-        return $result;
+    public function put($endpoint, $data){
+        return $this->call($endpoint, 'put', $data);
     }
     
-    public function getTaxRates(){
-        
-        $result = $this->get('/tax_rates');
-        return $result;
-    }
-    
-    public function getLedgerAccounts(){
-        
-        $result = $this->get('/ledger_accounts');
-        return $result;
-    }
-    
-    public function getServices(){
-        
-        $result = $this->get('/services');
-        return $result;
-    }
-    
+    public function delete($endpoint, $data){
+        return $this->call($endpoint, 'delete', $data);
+    }    
     
     /**************************************************************************
     * Private functions
@@ -124,19 +90,21 @@ class SageOne {
         // To-do: Validate endpoints
         // To-do: Validate types
         
-        // Data (if set) has to be array and then converted to json
-        if($data){
-            $data = json_encode($data);
-        }
-        
         $ch = curl_init();
         
         // Set curl url to call
         if($type == 'oauth'){
-            $curlURL = $this->oAuthAccessTokenURL;
+            $curlURL = $this->oAuthAccessTokenURL.'?'.http_build_query($data);
         } else {
             $curlURL = $this->apiUrl.$endpoint;
+            
+	        // Data (if set) has to be array and then converted to json
+	        if ($data){
+	            $data = json_encode($data);
+	        }
+                    
         }
+
         curl_setopt($ch, CURLOPT_URL, $curlURL);
         
         // Setup curl options
@@ -144,10 +112,12 @@ class SageOne {
             CURLOPT_CONNECTTIMEOUT => 10,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_TIMEOUT        => 60,
-            CURLOPT_USERAGENT      => 'SageOne-API-PHP'
+            CURLOPT_USERAGENT      => 'SageOne-API-PHP',
+            CURLOPT_FOLLOWLOCATION => true
         );
         
         switch($type){
+        
             case 'post':
                 $curl_options = $curl_options + array(
                     CURLOPT_HTTPHEADER => array(
@@ -158,8 +128,34 @@ class SageOne {
                     CURLOPT_POST        => 1,
                     CURLOPT_POSTFIELDS  => $data
                 );
-                break;
+            break;
                 
+            case 'put':
+                $curl_options = $curl_options + array(
+                	CURLOPT_CUSTOMREQUEST => 'PUT',
+                    CURLOPT_HTTPHEADER => array(
+                        'Accept: application/json',
+                        'Authorization: Bearer '.$this->accessToken,
+                        'Content-Type: application/json'
+                    ),
+                    CURLOPT_POST        => 1,
+                    CURLOPT_POSTFIELDS  => $data
+                );
+            break;
+                         
+            case 'delete':
+                $curl_options = $curl_options + array(
+                	CURLOPT_CUSTOMREQUEST => 'DELETE',
+                    CURLOPT_HTTPHEADER => array(
+                        'Accept: application/json',
+                        'Authorization: Bearer '.$this->accessToken,
+                        'Content-Type: application/json'
+                    ),
+                    CURLOPT_POST        => 1,
+                    CURLOPT_POSTFIELDS  => $data
+                );
+            break;
+                                                
             case 'get':
                 $curl_options = $curl_options + array(
                     CURLOPT_HTTPHEADER => array(
@@ -167,17 +163,16 @@ class SageOne {
                         'Authorization: Bearer '.$this->accessToken
                     )
                 );
-                break;
+            break;
                 
             case 'oauth':
                 $curl_options = $curl_options + array(
                     CURLOPT_HTTPHEADER => array('Accept: application/json'),
-                    CURLOPT_POST       => 1,
-                    CURLOPT_POSTFIELDS => $data
                 );
-                break;
+            break;
+            
         }
-        
+                
         // Set curl options
         curl_setopt_array($ch, $curl_options);
         
@@ -195,14 +190,7 @@ class SageOne {
         
         return json_decode($result, true);
     }
-    
-    private function get($endpoint, $data=false){
-        return $this->call($endpoint, 'get', $data);
-    }
-    
-    private function post($endpoint, $data){
-        return $this->call($endpoint, 'post', $data);
-    }
+            
 }
 
 ?>
